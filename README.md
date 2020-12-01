6917aa64524bc0f38833cabd486eff01b8c3480c
diegoferreirax <diegoferreirax95@gmail.com>
2020-12-01 10:19:49 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Diescript_DB
-Banco de dados em SQL Server
+
+Abaixo o diagrama de classes do banco de dados inicial da aplicação. Conforme o desenvolvimento haverá mais tabelas no banco.
+
+![](src/assets/images/diescript_db.PNG)
---
+++ src/queries/queries_diescript_db.sql
@@ -0,0 +1,53 @@
+use FinancasDB
+
+-- cadastro/busca categoria - Nome
+select * from Categoria
+
+insert into Categoria values ('Apartamento');
+insert into Categoria values ('Contas da casa');
+
+
+-- cadastro/busca usuario - Login/Senha/Nome/Salario/TipoContrato
+select * from Usuario
+
+insert into Usuario values ('ferreira', 'ferreira123', 'Ferreira', 3440, 'clt')
+
+
+-- cadastro/busca ano - Ano/AnoString/IdUsuarioRef
+select * from Ano
+select * from Ano WHERE IdUsuarioRef = 10002
+
+insert into Ano values ('2020-01-01', '2020', 10002)
+
+
+-- cadastro/busca mês - Mes/MesString/IdAnoRef
+select * from Mes
+
+insert into Mes values ('2020-01-01', '01', 10002)
+insert into Mes values ('2020-02-01', '02', 10002)
+insert into Mes values ('2020-03-01', '03', 10002)
+insert into Mes values ('2020-04-01', '04', 10002)
+insert into Mes values ('2020-05-01', '05', 10002)
+insert into Mes values ('2020-06-01', '06', 10002)
+insert into Mes values ('2020-07-01', '07', 10002)
+insert into Mes values ('2020-08-01', '08', 10002)
+insert into Mes values ('2020-09-01', '09', 10002)
+insert into Mes values ('2020-10-01', '10', 10002)
+insert into Mes values ('2020-11-01', '11', 10002)
+insert into Mes values ('2020-12-01', '12', 10002)
+
+
+-- cadastro/busca conta diversa - Nome/Valor/DataCompra/IdCategoriaRef_ContaDiversa/IdMesRef_ContaDiversa
+select * from ContaDiversa
+select * from ContaDiversa WHERE IdMesRef_ContaDiversa = 10003
+
+insert into ContaDiversa values ('Compra gelol', 15.55, '2020-02-01', 2, 10003)
+insert into ContaDiversa values ('Detergente', 5.99, '2020-02-01', 10003, 10003)
+
+
+-- cadastro/busca conta fixa - Nome/Valor/NumeroParcelas/DataVencimento/IdCategoriaRef_ContaFixa/IdMesRef_ContaFixa
+select * from ContaFixa
+select * from ContaFixa WHERE IdMesRef_ContaFixa = 10003
+
+insert into ContaFixa values ('Água', 25.78, 0, '2020-02-02', 1, 10003)
+insert into ContaFixa values ('Luz', 150.44, 0, '2020-02-15', 1, 10003)
